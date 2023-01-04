a226609f241a14729846bf0ed3d7cb5b42cda3de
tommilotta <tommaso.lotta@gmail.com>
2023-01-04 19:19:22 +0100

    Added masking function

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
+import cv2
 from keras.metrics import mean_absolute_error
-
-# IMG_SIZE = 224 # to be modified
 
 
 def load_filenames(file, path, path_p=None):
@@ -45,26 +44,54 @@ def load_filenames(file, path, path_p=None):
         df['image'] = path + '/' + df['image'].astype(str) + '.png'
 
     return df
+    
+
+def apply_masks(df, masks_path, img_size, num_images):
+    """
+    Apply masks to original images
+
+    Arguments:
+    df -- dataframe containing images paths
+    masks_path -- full path where images are placed
+    img_size -- size of the images
+    num_images -- number of images to which the masks have to be applied
+    """
+
+    for i in range(num_images):
+        im_path = df.loc[i, 'image']
+        im_name = im_path.split('/')[-1]
+        image = cv2.imread(im_path)
+        # mask_im = cv2.imread(path + '/' + masks_path + im_name, 0)
+        mask_im = cv2.imread(masks_path + im_name, 0)
+        # apply mask to original image
+        masked_im = cv2.bitwise_and(image, image, mask=mask_im)
+        # resize image
+        masked_im = cv2.resize(masked_im, (img_size, img_size), interpolation=cv2.INTER_NEAREST)
+        # overwrite the original image with its masked version
+        cv2.imwrite(im_path, masked_im)
 
 
-def mae_in_months(x, y):
+def mae_in_months(x, y, std, mean):
     """
     Return mean absolute error in months
 
     Arguments:
     x -- predicted value
     y -- ground-truth
+    std -- standard deviation of data
+    mean -- mean of data
     """
-    return mean_absolute_error((std_bone_age*x + mean_bone_age), (std_bone_age*y + mean_bone_age)) 
+    return mean_absolute_error((std*x + mean), (std*y + mean)) 
 
 
-def gen_2inputs(datagen, df, batch_size, shuffle=False, seed=None):
+def gen_2inputs(datagen, df, img_size, batch_size, shuffle=False, seed=None):
     """
     Merge images and gender in a single generator 
     
     Arguments:
     datagen -- ImageDataGenerator used for generating batches of augmented data
     df -- dataframe used to flow data into the generator
+    img_size -- size of the images
     batch_size -- size of the training batches
     suffle -- if True generator shuffles the data received
     seed -- required in order to have the same shuffling for image and gender data
@@ -72,11 +99,11 @@ def gen_2inputs(datagen, df, batch_size, shuffle=False, seed=None):
 
     gen_img = datagen.flow_from_dataframe(dataframe=df,
         x_col='image', y_col='boneage_n', batch_size=batch_size, seed=seed, shuffle=shuffle, class_mode='other',
-        target_size=(IMG_SIZE, IMG_SIZE), color_mode='grayscale', drop_duplicates=False)
+        target_size=(img_size, img_size), color_mode='grayscale', drop_duplicates=False)
     
     gen_gender = datagen.flow_from_dataframe(dataframe=df,
         x_col='image', y_col='gender(M/F)', batch_size=batch_size, seed=seed, shuffle=shuffle, class_mode='other',
-        target_size=(IMG_SIZE, IMG_SIZE), color_mode='grayscale', drop_duplicates=False)
+        target_size=(img_size, img_size), color_mode='grayscale', drop_duplicates=False)
     
     while True:
         X1i = gen_img.next()
